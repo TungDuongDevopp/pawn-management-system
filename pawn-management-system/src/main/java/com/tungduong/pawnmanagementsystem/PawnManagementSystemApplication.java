15ba2d8c78d19a06486568c1f697c020e3d501e5
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-22 19:29:13 +0700

    itimiate project

---
+++ pawn-management-system/src/main/java/com/tungduong/pawnmanagementsystem/PawnManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tungduong.pawnmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawnManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawnManagementSystemApplication.class, args);
+    }
+
+}
---
+++ pawn-management-system/src/test/java/com/tungduong/pawnmanagementsystem/PawnManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tungduong.pawnmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawnManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
