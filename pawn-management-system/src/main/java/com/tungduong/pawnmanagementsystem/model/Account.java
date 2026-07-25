67963259960941071fa9799e1639ff7bb8d186cb
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-25 23:21:20 +0700

    feat: add annotation model account,customer

--- pawn-management-system/src/main/java/com/tungduong/pawnmanagementsystem/model/Account.java
+++ pawn-management-system/src/main/java/com/tungduong/pawnmanagementsystem/model/Account.java
@@ -2,17 +2,22 @@ package com.tungduong.pawnmanagementsystem.model;
 
 import com.tungduong.pawnmanagementsystem.model.enums.AccountStatus;
 import com.tungduong.pawnmanagementsystem.model.enums.Role;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @ToString(exclude = "password")
 @Setter @Getter
+@Entity
+@Table(name = "accounts")
 public class Account {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
+    @Enumerated(EnumType.STRING)
     private Role role;
     AccountStatus status;
 
--- pawn-management-system/src/main/java/com/tungduong/pawnmanagementsystem/model/Customer.java
+++ pawn-management-system/src/main/java/com/tungduong/pawnmanagementsystem/model/Customer.java
@@ -1,10 +1,15 @@
 package com.tungduong.pawnmanagementsystem.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@Entity
+@Table(name = "customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
