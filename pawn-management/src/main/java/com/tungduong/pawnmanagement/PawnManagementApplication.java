c2e367334bd9b2b772c2ba920d7cf34ac034d9d2
TungDuong-Dev <devopptungduong@gmail.com>
2026-08-10 15:30:58 +0700

    Initial project pawnshop

---
+++ pawn-management/src/main/java/com/tungduong/pawnmanagement/PawnManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tungduong.pawnmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawnManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawnManagementApplication.class, args);
+    }
+
+}
---
+++ pawn-management/src/test/java/com/tungduong/pawnmanagement/PawnManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tungduong.pawnmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawnManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
