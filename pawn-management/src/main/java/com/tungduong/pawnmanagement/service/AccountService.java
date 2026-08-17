0d06604ca50c62d3b114beeef0906f6d5dc60ead
TungDuongDevopp <xaydaa8@gmail.com>
2026-08-17 20:47:06 +0700

    Check logic Account

--- pawn-management/src/main/java/com/tungduong/pawnmanagement/service/AccountService.java
+++ pawn-management/src/main/java/com/tungduong/pawnmanagement/service/AccountService.java
@@ -31,38 +31,30 @@ public class AccountService {
     private final RoleRepository roleRepository;
 
     private void ensureManipulable(Account account, Role role) {
-
         if (account != null &&
                 (account.getRecordStatus() == RecordStatus.DELETED
                         || account.getRecordStatus() == RecordStatus.INACTIVE
                         || account.getStatus() == AccountStatus.DELETED)) {
-
             throw new CanNotManipulateDataException(
                     "Account has been deleted or inactivated and cannot be manipulated"
             );
         }
-
         if (role != null &&
                 (role.getRecordStatus() == RecordStatus.DELETED
                         || role.getRecordStatus() == RecordStatus.INACTIVE)) {
-
             throw new CanNotManipulateDataException(
                     "Role has been deleted or inactivated and cannot be manipulated"
             );
         }
     }
 
-    public Page<AccountResponse> findAll(
-            Pageable pageable,
-            AccountFilterRequest filterRequest
-    ) {
+    public Page<AccountResponse> findAll(Pageable pageable, AccountFilterRequest filterRequest) {
         Specification<Account> specification = Specification.allOf(
                 AccountSpecification.statusNot(AccountStatus.DELETED),
                 AccountSpecification.hasRole(filterRequest),
                 AccountSpecification.hasStatus(filterRequest),
                 AccountSpecification.hasUsername(filterRequest)
         );
-
         return accountRepository.findAll(specification, pageable)
                 .map(accountMapper::toDto);
     }
@@ -76,66 +68,51 @@ public class AccountService {
     }
 
     public AccountResponse create(AccountRequest accountRequest) {
-
         if (accountRepository.existsByUsernameAndStatusNot(
                 accountRequest.getUsername(),
                 AccountStatus.DELETED
         )) {
             throw new DuplicateResourceException("Account already exists");
         }
-
         Long id = accountRequest.getRole().getId();
         String roleName = accountRequest.getRole().getName();
-
         Role role = roleRepository.findByIdOrName(id, roleName)
                 .orElseThrow(() ->
                         new ResourceNotFoundException("Role not found"));
-
         ensureManipulable(null, role);
-
         Account account = accountMapper.toEntity(accountRequest);
         account.setStatus(AccountStatus.ACTIVE);
         account.setRole(role);
-
         return accountMapper.toDto(accountRepository.save(account));
     }
 
     @Transactional
     public AccountResponse update(AccountUpdateRequest request, Long id) {
-
         Account currentAccount = accountRepository.findById(id)
                 .orElseThrow(() ->
                         new ResourceNotFoundException("Account not found"));
 
         Role role = null;
-
         if (request.getRole() != null) {
             role = roleRepository.findById(request.getRole().getId())
                     .orElseThrow(() ->
                             new ResourceNotFoundException("Role not found"));
         }
-
-        // Check Account + Role cùng một chỗ
         ensureManipulable(currentAccount, role);
-
         if (role != null) {
             currentAccount.setRole(role);
         }
-
         if (request.getStatus() != null) {
             currentAccount.setStatus(request.getStatus());
         }
-
         return accountMapper.toDto(currentAccount);
     }
 
     @Transactional
     public void deleteById(Long id) {
-
         Account account = accountRepository.findById(id)
                 .orElseThrow(() ->
                         new ResourceNotFoundException("Account not found"));
-
         ensureManipulable(account, null);
         account.setStatus(AccountStatus.DELETED);
         account.setRecordStatus(RecordStatus.DELETED);
