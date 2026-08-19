8f6cce6fd1b9828b2d3c005f197633baab7b3608
TungDuongDevopp <xaydaa8@gmail.com>
2026-08-19 19:47:22 +0700

    File Service

---
+++ pawn-management/src/main/java/com/tungduong/pawnmanagement/helper/exception/InvalidTypeFileException.java
@@ -0,0 +1,7 @@
+package com.tungduong.pawnmanagement.helper.exception;
+
+public class InvalidTypeFileException extends RuntimeException{
+    public InvalidTypeFileException(String message) {
+        super(message);
+    }
+}
